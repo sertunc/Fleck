25bb7ed88bae6e1840a07a7ce78b4227f023ef32
cahitsertunc.selen <cahitsertunc.selen@CSSELEN>
2022-08-24 16:50:19 +0300

    add certificate checker layer

---
+++ src/Fleck.CertificateChecker/Checkers/ContentMatchChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Fleck.CertificateChecker.Checkers
+{
+    public class ContentMatchChecker : IChecker
+    {
+        public bool Check(X509Certificate2 certificate1, X509Certificate certificate2)
+        {
+            byte[] certificate1Bytes = certificate1.GetRawCertData();
+            byte[] certificate2Bytes = certificate2.GetRawCertData();
+
+
+            for (int i = 0; i < certificate1Bytes.Length; i++)
+            {
+                if (certificate1Bytes[i] != certificate2Bytes[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
