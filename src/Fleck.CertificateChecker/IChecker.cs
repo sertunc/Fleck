4658dead31b4c0d89f44948adda6d6da83e88ed8
cahitsertunc.selen <cahitsertunc.selen@CSSELEN>
2022-08-27 23:24:29 +0300

    remove certificate checker layer

--- src/Fleck.CertificateChecker/IChecker.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Fleck.CertificateChecker
-{
-    public interface IChecker
-    {
-        bool Check(X509Certificate2 certificate1, X509Certificate certificate2);
-    }
-}
